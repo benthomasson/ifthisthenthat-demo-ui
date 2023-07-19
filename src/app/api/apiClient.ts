77133bd65bef9ddc3cecbcb1b33872b8d7cb98a3
Ben Thomasson <bthomass@redhat.com>
2023-07-19 15:23:12 -0400

    Use location.hostname instead of localhost for API

--- src/app/api/apiClient.ts
+++ src/app/api/apiClient.ts
@@ -1,7 +1,7 @@
 import axios from 'redaxios';
 
 const apiClient = axios.create({
-  baseURL: 'http://localhost:8000',
+  baseURL: 'http://' + location.hostname + ':8000',
 });
 
 export { apiClient };
